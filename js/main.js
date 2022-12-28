827934a2ee12206a7eff401a9f9e4fc1f58c9ea3
Frorex Studio <81986912+FrorexStudio@users.noreply.github.com>
2022-12-28 19:19:28 +0200

    Start

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function(){
+    $('.primary-divider__text').marquee({
+  
+    //Если вы хотите всегда анимировать с помощью jQuery
+    allowCss3Support: true,
+  
+    //работает, когда allowCss3Support имеет значение true - полный список см.http://www.w3.org/TR/2013/WD-css3-transitions-20131119/#transition-timing-function
+    css3easing: 'linear',
+  
+    //Требуется плагин JQuery. По умолчанию это «линейный»
+    easing: 'linear',
+  
+    //время паузы до следующего поворота анимации в миллисекундах
+    // delayBeforeStart: 0,
+    //'left', 'right', 'up' or 'down'
+    direction: 'left',
+  
+    //true или false - следует ли дублировать выделение, чтобы показать эффект продолжения потока
+    duplicated: true,
+  
+    //скорость в миллисекундах
+    duration: 5000,
+  
+    //разрыв в пикселях между тикерами
+    gap: 30,
+  
+    //пауза
+    pauseOnCycle: false,
+  
+    //при наведении курсора на паузу - с помощью плагина jQuery https://github.com/tobia/Pause
+    pauseOnHover: false,
+  
+    startVisible: true
+    
+    });
+  });
